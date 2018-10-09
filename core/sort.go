f0ec401c916b72f6ddaefcb491377ec7af77306f
fengpengfei <fengpengfei@bilibili.com>
2018-10-09 22:32:24 +0800

    add select sort

--- core/sort.go
+++ core/sort.go
@@ -30,3 +30,30 @@ func BubbleSort(data interface{}) {
 		}
 	}
 }
+
+func SelectionSort(data interface{}) {
+	d := data.([]interface{})
+	for i := 0; i < len(d)-1; i++ {
+		min := i
+		for j := i + 1; j < len(d); j++ { //search the min elem
+			switch d[j].(type) {
+			case int:
+				m := d[j].(int)
+				n := d[min].(int)
+				if m < n {
+					min = j //save the min elem index
+				}
+			case string:
+				m := d[j].(string)
+				n := d[min].(string)
+				if m < n {
+					min = j
+				}
+			}
+		}
+		//keep order zone by min
+		t := d[i]
+		d[i] = d[min]
+		d[min] = t
+	}
+}
--- tools/random.go
+++ tools/random.go
@@ -1,1 +1,50 @@
 package tools
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type (
+	Random struct {
+	}
+)
+
+var (
+	global = New()
+)
+
+func New() *Random {
+	rand.Seed(time.Now().UnixNano())
+	return new(Random)
+}
+
+func (r *Random) Strings() (s []string) {
+	s = make([]string, 0)
+	for i := 97; i < 123; i++ {
+		s = append(s, fmt.Sprintf("%c ", rand.Intn(i)))
+	}
+	return
+}
+
+func (r *Random) Numbers(start int, end int, count int) []int {
+	if end < start || (end-start) < count {
+		return nil
+	}
+	nums := make([]int, 0)
+	for len(nums) < count {
+		num := rand.Intn((end - start)) + start
+		exist := false
+		for _, v := range nums {
+			if v == num {
+				exist = true
+				break
+			}
+		}
+		if !exist {
+			nums = append(nums, num)
+		}
+	}
+	return nums
+}
