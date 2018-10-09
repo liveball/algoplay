42880d4d8187fdd3f256273cbc7b98e88fe02478
fengpengfei <fengpengfei@bilibili.com>
2018-10-09 22:49:20 +0800

    update select

--- core/sort/selection_sort.go
+++ core/sort/selection_sort.go
@@ -7,22 +7,23 @@ import (
 func SelectionSort(data interface{}) interface{} {
 	arr := data.([]interface{})
 	length := len(arr)
-	minIndex := 0
-	tmp := 0
-	for i_k, _ := range arr {
-		switch arr[i_k].(type) {
-		case int:
-			minIndex = i_k
 
-			for j := i_k + 1; j < length; j++ {
+	for i := range arr {
+		minIndex := 0
+		tmp := 0
+		switch arr[i].(type) {
+		case int:
+			minIndex = i
+
+			for j := i + 1; j < length; j++ {
 				m := arr[j].(int)
 				n := arr[minIndex].(int)
 				if m > n {
 					minIndex = j
 				}
 			}
-			tmp = arr[i_k].(int)
-			arr[i_k] = arr[minIndex]
+			tmp = arr[i].(int)
+			arr[i] = arr[minIndex]
 			arr[minIndex] = tmp
 		}
 
