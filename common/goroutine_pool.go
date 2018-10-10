d6d75d9b3ec66fb9e59ac4c4d88c99a26d66934f
wei fuchuan <fuchuan.wei@hotmail.com>
2018-10-10 10:36:16 +0800

    update goroutine pool

--- common/goroutine_pool.go
+++ common/goroutine_pool.go
@@ -10,8 +10,7 @@ type Actor = func()
 
 // Pool is groutine pool
 type Pool struct {
-	count    int             // goroutine count
-	channels []chan Actor    //
+	count    int             // goroutine count 
 	recv     chan Actor      // receive actor
 	wg       *sync.WaitGroup // to wait all goroutine complete
 }
@@ -22,38 +21,19 @@ func NewPool(count int) *Pool {
 		count = runtime.NumCPU()
 	} 
 	pool := &Pool{
-		count:    count,
-		channels: []chan Actor{},
+		count:    count, 
 		recv:     make(chan Actor, count),
 		wg:       &sync.WaitGroup{},
 	}
 	pool.wg.Add(count)
-	for i := 0; i < count; i++ {
-		ch := make(chan Actor)
+	for i := 0; i < count; i++ { 
 		go func() {
-			for actor := range ch {
+			for actor := range pool.recv {
 				actor()
 			}
 			pool.wg.Done()
-		}()
-		pool.channels = append(pool.channels, ch)
-	}
-	go func() {
-		i := 0
-		for actor := range pool.recv {
-			for {
-				select {
-				case pool.channels[i] <- actor:
-					break
-				default:
-				}
-				i++
-				if i == pool.count {
-					i = 0
-				}
-			}
-		}
-	}()
+		}() 
+	}  
 	return pool
 }
 
@@ -64,10 +44,7 @@ func (p *Pool) Go(actor Actor) {
 
 // Close the pool
 func (p *Pool) Close() {
-	close(p.recv)
-	for _, c := range p.channels {
-		close(c)
-	}
+	close(p.recv) 
 }
 
 // CloseAndWaitAll close the pool and wait all goroutine completed
