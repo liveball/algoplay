9fe8fe73631f7b9c21a1269fb97e00c76398a8f8
fengpengfei <fengpengfei@bilibili.com>
2020-11-04 22:46:51 +0800

    up quick sort

---
+++ leetcode/sort/merge.go
@@ -0,0 +1,54 @@
+package sort
+
+func mergeSort(a []int) {
+	mergeSortA(a, 0, len(a)-1)
+}
+
+func mergeSortA(a []int, p int, r int) {
+	if p >= r {
+		return
+	}
+	//取p,r 中间的位置q
+	q := (p + r) / 2
+	//分治递归
+	mergeSortA(a, p, q)
+	mergeSortA(a, q+1, r)
+
+	//将a[p...q] 和 a[q+1...r] 合并为a[p...r]
+
+	merge(a, p, q, r)
+}
+
+func merge(a []int, p, q, r int) {
+	i, j, k := p, q+1, 0
+	tmp := make([]int, r-p+1)
+
+	for i <= q && j <= r {
+		if a[i] <= a[j] {
+			tmp[k] = a[i]
+			i++
+		} else {
+			tmp[k] = a[j]
+			j++
+		}
+		k++
+	}
+
+	//查看a[p...q] 剩余
+	for ; i <= q; i++ {
+		tmp[k] = a[i]
+		k++
+	}
+	//查看a[q+1...r] 剩余
+	for ; j <= r; j++ {
+		tmp[k] = a[j]
+		k++
+	}
+
+	//拷贝 tmp[p:r-p] 到a[p:r]
+	//for n := p; n <= r-p; n++ {
+	//	a[n] = tmp[n]
+	//}
+
+	copy(a[p:r+1], tmp)
+}
--- leetcode/sort/quick.go
+++ leetcode/sort/quick.go
@@ -1,39 +1,1 @@
-package sort
-
-func QuickSort(nums []int) {
-	n := len(nums)
-	if n <= 1 {
-		return
-	}
-
-	quickSort(nums, 0, n-1)
-}
-
-func quickSort(nums []int, l, r int) {
-	if l >= r {
-		return
-	}
-
-	m := partition(nums, l, r)
-
-	quickSort(nums, l, m-1)
-	quickSort(nums, m+1, r)
-
-}
-
-func partition(nums []int, l, r int) int {
-	pivot := nums[r] //假设中心点数据为当前数组第r个元素
-	nl := l
-
-	for i := l; i < r; i++ {
-		if nums[i] < pivot { //如果当前元素小于中心点数据，则将 中心点左边元素和当前元素进行交换, nl+1
-			nums[i], nums[l] = nums[l], nums[i]
-			nl++
-		}
-	}
-
-	nums[nl], nums[r] = nums[r], nums[nl]
-
-	return nl
-
-}
+package sortfunc quickSort(nums []int) {	n := len(nums)	if n <= 1 {		return	}	quickSortA(nums, 0, n-1)}func quickSortA(nums []int, l, r int) {	if l >= r {		return	}	m := partition(nums, l, r)	quickSortA(nums, l, m-1)	quickSortA(nums, m+1, r)}func partition(nums []int, l, r int) int {	pivot := nums[r] //假设分区点数据为当前数组第r个元素	nl := l	for i := l; i < r; i++ {		if nums[i] < pivot { //如果当前元素小于分区点数据，则将 分区点左边元素和当前元素进行交换, nl+1			nums[i], nums[l] = nums[l], nums[i]			nl++		}	}	nums[nl], nums[r] = nums[r], nums[nl]//交换分区点	return nl}
