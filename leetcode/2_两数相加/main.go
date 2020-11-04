7e825bceab97ca43cd02baa45152f2e230d9a6fd
fengpengfei <fengpengfei@bilibili.com>
2020-11-05 00:39:29 +0800

    up

--- leetcode/2_两数相加/main.go
+++ leetcode/2_两数相加/main.go
@@ -33,13 +33,13 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	if l1 == nil && l2 == nil{
-		return l2
+	if l1 == nil && l2 == nil {
+		return nil
 	}
 
 	p, q := l1, l2
 	var dummy = &ListNode{}
-	cur:=dummy
+	cur := dummy
 
 	var carry int
 
