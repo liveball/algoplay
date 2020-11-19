aaaa77787b2d77ef7e98df548b9a4c037094e2f8
mountainfeng <mountainfpf@gmail.com>
2020-11-19 19:00:01 +0800

    ad heap for top k

--- leetcode/215. 数组中的第K个最大元素/main.go
+++ leetcode/215. 数组中的第K个最大元素/main.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println(findKthLargest([]int{3, 2, 1, 5, 6, 4}, 2))          //5
 	fmt.Println(findKthLargest([]int{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4)) //4
+
+	fmt.Println(findKthLargestMaxHeap([]int{3, 2, 1, 5, 6, 4}, 2))          //5
+	fmt.Println(findKthLargestMaxHeap([]int{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4)) //4
 }
 
 // func findKthLargest(nums []int, k int) int {
@@ -40,6 +43,51 @@ func partition(nums []int, l, r int) int {
 
 	nums[i], nums[r] = nums[r], nums[i]
 	return i
+}
+
+//构造大顶堆，做k-1次删除，对顶元素就是要找的第k大元素
+func findKthLargestMaxHeap(nums []int, k int) int {
+	heapSize := len(nums)
+	//构造大顶堆,使得堆顶最大元素是nums[0]
+	buildMaxHeap(nums, heapSize)
+
+	//删除k-1次堆顶元素，注意每次循环len(nums) 会减少一个元素
+	for i := len(nums) - 1; i > len(nums)-k; i-- {
+		//交换堆顶元素和最后一个节点的元素，使堆顶元素交换到最后，数组长度减1删除
+		nums[0], nums[i] = nums[i], nums[0]
+		//堆元素数量减1
+		heapSize--
+		//每次从堆顶元素开始堆化
+		heapify(nums, 0, heapSize)
+	}
+
+	return nums[0] //返回堆顶元素
+}
+
+func buildMaxHeap(nums []int, heapSize int) {
+	for i := len(nums) / 2; i >= 0; i-- {
+		heapify(nums, i, heapSize)
+	}
+}
+
+func heapify(nums []int, i, heapSize int) {
+	l := 2*i + 1
+	r := 2*i + 2
+	max := i
+
+	if l < heapSize && nums[max] < nums[l] {
+		max = l
+	}
+	if r < heapSize && nums[max] < nums[r] {
+		max = r
+	}
+
+	if i != max {
+		//交换堆顶元素为最大的元素
+		nums[i], nums[max] = nums[max], nums[i]
+		//递归堆化
+		heapify(nums, max, heapSize)
+	}
 }
 
 // 215. 数组中的第K个最大元素
