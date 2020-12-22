a0d81155b804c3ade86cdf852868e82538974765
fengpf <18221167541@163.com>
2020-12-22 19:52:49 +0800

    up heap sort

--- core/tree/heap/heap.go
+++ core/tree/heap/heap.go
@@ -88,22 +88,34 @@ func swap(a []int, i, j int) {
 	a[j] = t
 }
 
-func HeapSort(a []int, n int) {
-	BuildHeap(a, n)
+func HeapSort(a []int) {
+	heapSize := len(a)
+
+	BuildMinHeap(a, heapSize)
+	// BuildMaxHeap(a, heapSize)
 
 	fmt.Println("after build", a)
 
-	for i := n; i >= 0; i-- {
-		swap(a, 0, i)         //交换 堆顶元素与最后一个元素
-		minHeapify(a, 0, i-1) //堆化 小顶堆 i-1 每次从堆顶开始堆化，且元素个数减1
-		//maxHeapify(a, 0, i-1) //堆化 大顶堆
+	for i := len(a) - 1; i > 0; i-- {
+		swap(a, 0, i) //交换 堆顶元素与最后一个元素
+		heapSize--
+
+		minHeapify(a, 0, heapSize) //堆化 小顶堆 i-1 每次从堆顶开始堆化，且元素个数减1
+		// maxHeapify(a, 0, heapSize) //堆化 大顶堆
+
+		fmt.Println(a)
 	}
 }
 
-func BuildHeap(a []int, n int) {
+func BuildMinHeap(a []int, n int) {
 	for i := n / 2; i >= 0; i-- {
 		minHeapify(a, i, n) //每次从i节点往下开始堆化，且元素个数不变
-		//maxHeapify(a, i, n)
+	}
+}
+
+func BuildMaxHeap(a []int, n int) {
+	for i := n / 2; i >= 0; i-- {
+		maxHeapify(a, i, n)
 	}
 }
 
@@ -123,11 +135,11 @@ func minHeapify(a []int, i int, count int) { //a:数组，i:父节点位置，co
 	for {
 		minPos = i
 
-		if 2*i <= count && a[i] < a[2*i] {
+		if 2*i < count && a[minPos] > a[2*i] {
 			minPos = 2 * i
 		}
 
-		if 2*i+1 <= count && a[minPos] < a[2*i+1] {
+		if 2*i+1 < count && a[minPos] > a[2*i+1] {
 			minPos = 2*i + 1
 		}
 
@@ -140,18 +152,17 @@ func minHeapify(a []int, i int, count int) { //a:数组，i:父节点位置，co
 	}
 }
 
-func maxHeapify(a []int, top int, count int) {
+func maxHeapify(a []int, i int, count int) {
 	var maxPos int
 
-	for i := top; i <= count/2; {
-
+	for {
 		maxPos = i
 
-		if 2*i <= count && a[i] > a[2*i] {
+		if 2*i < count && a[maxPos] < a[2*i] {
 			maxPos = 2 * i
 		}
 
-		if 2*i+1 <= count && a[maxPos] > a[2*i+1] {
+		if 2*i+1 < count && a[maxPos] < a[2*i+1] {
 			maxPos = 2*i + 1
 		}
 
--- go.mod
+++ go.mod
@@ -4,11 +4,8 @@ go 1.12
 
 require (
 	github.com/davecgh/go-spew v1.1.1
-	github.com/kr/pretty v0.1.0 // indirect
 	github.com/ramya-rao-a/go-outline v0.0.0-20200117021646-2a048b4510eb // indirect
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/tools v0.0.0-20201210161338-b71f123c27e9 // indirect
 	golang.org/x/tools/gopls v0.5.5 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
-	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
