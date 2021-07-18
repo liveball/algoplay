9f8a11971e2a134765f87158aca06667a55c449b
fengpf <18221167541@163.com>
2021-07-18 12:49:10 +0800

    up

--- leetcode_local/go/53.最大子序和.go
+++ leetcode_local/go/53.最大子序和.go
@@ -6,11 +6,13 @@
 
 // @lc code=start
 func maxSubArray(nums []int) int {
-	return test1(nums)
-	// return test2(nums)
+	return maxSubArray1(nums)
+	// return maxSubArray2(nums)
+	// return maxSubArray3(nums)
+	// return maxSubArray4(nums)
 }
 
-func test1(nums []int) int {
+func maxSubArray1(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
@@ -27,7 +29,7 @@ func test1(nums []int) int {
 	return res
 }
 
-func test2(nums []int) int {
+func maxSubArray2(nums []int) int {
 	res := -1 << 31
 
 	if len(nums) == 0 {
@@ -59,6 +61,46 @@ func max(a, b int) int {
 	}
 
 	return b
+}
+
+// 解法一 DP
+func maxSubArray3(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	dp, res := make([]int, len(nums)), nums[0]
+	dp[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		if dp[i-1] > 0 {
+			dp[i] = nums[i] + dp[i-1]
+		} else {
+			dp[i] = nums[i]
+		}
+		res = max(res, dp[i])
+	}
+	return res
+}
+
+// 解法二 模拟
+func maxSubArray4(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	maxSum, res, p := nums[0], 0, 0
+	for p < len(nums) {
+		res += nums[p]
+		if res > maxSum {
+			maxSum = res
+		}
+		if res < 0 {
+			res = 0
+		}
+		p++
+	}
+	return maxSum
 }
 
 // @lc code=end
