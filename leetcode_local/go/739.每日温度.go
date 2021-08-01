76840a5b6bb16e863937949008182ad049ebd1e3
fengpf <18221167541@163.com>
2021-08-01 18:41:13 +0800

    up

--- leetcode_local/go/739.每日温度.go
+++ leetcode_local/go/739.每日温度.go
@@ -6,6 +6,26 @@
 
 // @lc code=start
 func dailyTemperatures(temperatures []int) []int {
+	// return temperatures1(temperatures)
+	return temperatures2(temperatures)
+}
+
+//O(N^2)
+func temperatures1(T []int) []int {
+	res := make([]int, len(T))
+	for i := 0; i < len(T); i++ {
+		for j := i + 1; j < len(T); j++ {
+			if T[j] > T[i] {
+				res[i] = j - i
+				break
+			}
+		}
+	}
+	return res
+}
+
+//O(N)
+func temperatures2(temperatures []int) []int {
 	if len(temperatures) == 0 {
 		return []int{}
 	}
